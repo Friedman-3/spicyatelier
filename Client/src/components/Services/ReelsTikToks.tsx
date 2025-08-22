80ceedaf6d8389e9c57daaf7e2e936d02ae935cd
Sammir Villca <h.sammir.10@gmail.com>
2025-08-21 22:04:17 -0400

    arreglos rutas

--- Client/src/components/Services/ReelsTikToks.tsx
+++ Client/src/components/Services/ReelsTikToks.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ReelsTikToks = () => {
+const ReelsTiktoks = () => {
   return (
     <section className="min-h-screen bg-gradient-to-br from-red-900 via-red-800 to-red-900 relative overflow-hidden">
       {/* Elementos decorativos de fondo */}
@@ -253,4 +253,4 @@ const ReelsTikToks = () => {
   );
 };
 
-export default ReelsTikToks;
+export default ReelsTiktoks;
